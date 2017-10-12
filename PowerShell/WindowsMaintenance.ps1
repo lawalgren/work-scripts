7f9300d2b766c14b17cf81277b1c7d4b4f80f2c4
Lucas Walgren <lawalgren@mavs.coloradomesa.edu>
2017-10-11 22:43:07 -0600

    Powershell script works, ducky script almost does.

--- PowerShell/WindowsMaintenance.ps1
+++ PowerShell/WindowsMaintenance.ps1
@@ -1,15 +1,42 @@
+<#
+Change the values of these variables to "yes"
+to include that particular part of the script in the runtime.
+$updates = "yes"  : check for windows updates.
+$defrag = "yes"   : defragment system volume
+$chkdsk = "yes"   : schedule a disk check on the next restart for the system volume
+$ccleaner = "yes" : install, run, and uninstall CCleaner
+$java = "yes"     : download and install Java
+$adobe = "yes"    : download and install Adobe Flash Player and Reader
+$clrtemp = "yes"  : manually delete contents of %tmp% folder
+#>
+
+$updates = "no"
+$chkdsk = "no"
+$ccleaner = "no"
+$java = "no"
+$adobe = "yes"
+$clrtemp = "no"
+$defrag = "no"
+
+<#
+If the links for the latest versions of the software get old,
+you can change them here.
+$CCLink : CCleaner
+$javaLink : Java
+$FPNPAPILink : Flash Player for NPAPI (Firefox)
+$FPAXLink : Flash Player for ActiveX (Internet Explorer)
+$ReaderLink : Adobe Reader
+#>
+
 $CCLink = "http://download.piriform.com/ccsetup535.exe"
-
 $javaLink = "http://javadl.oracle.com/webapps/download/AutoDL?BundleId=225355_090f390dda5b47b9b721c7dfaa008135"
-
 $FPNPAPILink = "https://fpdownload.macromedia.com/pub/flashplayer/latest/help/install_flash_player.exe"
-
 $FPAXLink = "https://fpdownload.macromedia.com/pub/flashplayer/latest/help/install_flash_player_ax.exe"
-
-$ReaderLink = "https://admdownload.adobe.com/bin/live/readerdc_en_xa_cra_install.exe"
+$ReaderLink = "http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/1701220093/AcroRdrDC1701220093_en_US.exe"
 
 $wc = New-Object System.Net.WebClient
 
+#determine windows version
 if(([System.Environment]::OSVersion.Version).major -eq "10") {
   $version = "10"
 } elseif (([System.Environment]::OSVersion.Version).major -ge "6.1") {
@@ -18,14 +45,6 @@ if(([System.Environment]::OSVersion.Version).major -eq "10") {
   echo "Can't support this version of windows."
   return
 }
-
-$updates = "no"
-$defrag = "no"
-$chkdsk = "no"
-$ccleaner = "no"
-$java = "no"
-$adobe = "no"
-$clrtemp = "no"
 
 #Start checking for updates
 if($updates -eq "yes") {
@@ -44,12 +63,6 @@ if($defrag -eq "yes") {
   cmd.exe /c "defrag %systemdrive%"
 }
 
-#chkdsk main disk on restart
-if($chkdsk -eq "yes") {
-  echo "Will perform a checkdisk on next restart"
-  cmd.exe /c "echo y | chkdsk %systemdrive% /f /r"
-}
-
 #install and run CCleaner
 if($ccleaner -eq "yes") {
   echo "Downloading CCleaner..."
@@ -61,6 +74,7 @@ if($ccleaner -eq "yes") {
   cmd.exe /c "CCleaner64 /Auto"
 }
 
+#install java
 if($java -eq "yes") {
   echo "Downloading Java..."
   $wc.DownloadFile($javaLink, "$ENV:TEMP\java.exe");
@@ -69,20 +83,25 @@ if($java -eq "yes") {
   cmd.exe /c "java.exe INSTALL_SILENT=Enable REMOVEOUTOFDATEJRES=1"
 }
 
+#install latest versions of flash and reader
 if($adobe -eq "yes") {
 echo "Downloading Flash Player NPAPI..."
 $wc.DownloadFile($FPNPAPILink, "$ENV:TEMP\install_flash_player.exe");
 echo "Downloading Flash Player ActiveX..."
 $wc.DownloadFile($FPAXLink, "$ENV:TEMP\install_flash_player_ax.exe");
 echo "Downloading Adobe Reader..."
-$wc.DownloadFile($ReaderLink, "$ENV:TEMP\readerdc_en_xa_cra_install.exe");
+$wc.DownloadFile($ReaderLink, "$ENV:TEMP\AcroRdrDC1701220093_en_US.exe");
 
-echo "Installing Flash Player and Reader..."
+cd $ENV:TEMP
+echo "Installing Flash Player NPAPI..."
 cmd.exe /c "install_flash_player.exe -install"
+echo "Installing Flash Player ActiveX..."
 cmd.exe /c "install_flash_player_ax.exe -install"
-cmd.exe /c "readerdc_en_xa_cra_install.exe /sALL /rs"
+echo "Installing Adobe Reader DC..."
+cmd.exe /c "AcroRdrDC1701220093_en_US.exe /sAll /rs"
 }
 
+#cleanup
 if($ccleaner -eq "yes") {
 echo "Uninstalling CCleaner..."
 cd $ENV:PROGRAMW6432\CCleaner
@@ -93,6 +112,12 @@ if($clrtemp -eq "yes") {
 echo "Clearing the temp folder..."
 cd $ENV:TEMP
 cmd.exe /c "del * /S /Q"
+}
+
+#chkdsk main disk on restart
+if($chkdsk -eq "yes") {
+  echo "Will perform a checkdisk on next restart"
+  cmd.exe /c "echo y | chkdsk %systemdrive% /f /r"
 }
 
 echo "Done with script."
